9168ccb5c5649dc4169044eb155204e7684fa4ca
Anil Bhujel <abhujel055@gmail.com>
2021-09-10 22:37:57 +0545

    index

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+import './index.css'
+
+ReactDom.render(
+
+    <App
+        name='Simple Calculator'
+
+    />,
+    document.getElementById("root")
+)
+
+
